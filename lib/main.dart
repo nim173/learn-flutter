7c92b4821a80c74e3b6946ec02fd519c3e2b0b40
Nimesh <nchathuranga173@gmail.com>
2022-03-19 22:32:08 +1100

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,52 +4,111 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-              color: Colors.blue,
-              height: 300,
-              width: 300,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    height: 40,
-                    width: 80,
+        appBar: AppBar(
+          title: const Text("ListView in Flutter"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: const [
+                  Text(
+                    'Peter',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
                   ),
-                  Container(
-                    color: Colors.purple,
-                    height: 40,
-                    width: 80,
+                  Text(
+                    'Paul',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
                   ),
-                  Container(
-                    color: Colors.orange,
-                    height: 40,
-                    width: 80,
+                  Text(
+                    'John',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
                   ),
-                  const SizedBox(height: 20), // add space
-                  Row(
-                    textBaseline: TextBaseline.alphabetic,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    children: const [
-                      Text(
-                        'US\$',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        '...........................',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        '\$3000',
-                        style: TextStyle(fontSize: 30),
-                      ),f
-                    ],
-                  )
+                  Text(
+                    'Peter',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'Paul',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'John',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'Peter',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'Paul',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'John',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'Peter',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'Paul',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    'John',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
                 ],
-              )),
+              ),
+            ),
+            const Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 70,
+                color: Colors.black,
+              ),
+            ),
+          ],
         ),
       ),
     ),
